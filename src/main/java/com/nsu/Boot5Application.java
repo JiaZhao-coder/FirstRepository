f0f557554e2b2e280f714065a34f19d2c6eb9ade
JiaZhao-coder <JiaZhao********>
2020-09-22 20:43:54 +0800

    贾钊第一次提交boot5项目代码

---
+++ src/main/java/com/nsu/Boot5Application.java
@@ -0,0 +1,13 @@
+package com.nsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot5Application.class, args);
+    }
+
+}
