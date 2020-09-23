d978309367a6715449e9acdf8babbf073598b054
JiaZhao-coder <JiaZhao********>
2020-09-23 11:04:21 +0800

    贾钊第2次提交boot5项目代码

--- src/main/java/com/nsu/controller/HelloController.java
+++ src/main/java/com/nsu/controller/HelloController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    //开发人员 李民志提交了代码
-    //开发人员贾钊 第二次提交了代码
+
+    //开发人员贾钊 第1次提交了代码
+    //开发人员贾钊 第2次提交了代码
+
 
 }
